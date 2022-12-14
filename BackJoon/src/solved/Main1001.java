fbfcb1bfc7a575801526cfe3fd0857a1c13921d6
EzenNene <kkhj0822@gmail.com>
2022-12-15 00:17:42 +0900

    1215 집

---
+++ BackJoon/src/solved/Main1001.java
@@ -0,0 +1,14 @@
+package solved;
+import java.util.Scanner;
+
+public class Main1001 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+
+		System.out.print(A-B);
+	}
+
+}
---
+++ BackJoon/src/solved/Main1008.java
@@ -0,0 +1,14 @@
+package solved;
+import java.util.Scanner;
+
+public class Main1008 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double A = scanner.nextInt();
+		double B = scanner.nextInt();
+
+		System.out.print(A/B);
+	}
+
+}
